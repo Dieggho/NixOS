57acbca8456a961c02580736ab124bd1b240cc49
Diëggho <diegghooliver@gmail.com>
2024-06-04 10:35:22 -0300

    Create hardware-configuration.nix

---
+++ etc/nixos/hardware-configuration.nix
@@ -0,0 +1,39 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+  boot.kernelParams = [ "quiet""i915.force_probe=5917" "udev.log_level=3" "acpi_backlight=video" "fbcon=nodefer" "video=eDP-1:1920x1080@60" "intel_iommu=igfx_off" "enable_dc=2" "enable_fbc=1" "fastboot=1" "acpi_rev_override=5" "i915.enable_guc=2" ];
+  
+ fileSystems."/" =
+    { device = "/dev/disk/by-uuid/";
+      fsType = "ext4";
+    };
+
+  boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/";
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
