ec3e48c6212c84a0c79d2788a0035587c3bb9a21
Diëggho <diegghooliver@gmail.com>
2026-01-26 20:09:36 -0300

    Add files via upload

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -1,13 +1,8 @@
-
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
     ];
 
@@ -16,26 +11,52 @@
   boot.initrd.systemd.enable = true;
   boot.plymouth.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  networking.hostName = "t480"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  
+  # Hardening
+  boot.kernel.sysctl = {
+  "kernel.randomize_va_space" = 2;
+  "vm.mmap_rnd_bits" = 32;
+  "vm.mmap_rnd_compat_bits" = 16;
+  };
 
+  nixpkgs.config.hardening = {
+  enable = true;
+  pie = true;
+  relro = "full";
+  now = true;
+  stackprotector = true;
+  fortify = true;
+  };
+
+  security.apparmor.enable = true;
+
+  # Enable Wifi
+  networking.hostName = "t480"; # Define your hostname.
+  networking.wireless.iwd.enable = true;
+ 
   # You can choose your kernel simply by setting the boot.kernelPackages option 
   boot.kernelPackages = pkgs.linuxPackages_latest;
   
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # ThinkPad battery + power
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_BAT="powersave";
+      CPU_SCALING_GOVERNOR_ON_AC="performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT="power";
+      CPU_ENERGY_PERF_POLICY_ON_AC="performance";
+      PLATFORM_PROFILE_ON_BAT="low-power";
+      PLATFORM_PROFILE_ON_AC="performance";
+    };
+  };
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+  # No X11
+  services.xserver.enable = false;
 
   # Enable Bluetooth
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
   services.blueman.enable = true;
-
-  # Enable nix-command and flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];  
 
   # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
@@ -56,11 +77,12 @@
   };
 
   # Enable services
+  services.seatd.enable = true;
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-       command = "${pkgs.cage}/bin/cage -d -s -- gtkgreet -b /home/void/.local/share/backgrounds/NixOS.png";
+       command = "${pkgs.cage}/bin/cage -d -s -- gtkgreet -b /home/void/.local/share/Wallpapers/000-NixOS.png";
        user = "void";
       };
     };
@@ -70,48 +92,53 @@
     Hyprland
   '';
 
-  services.logind.lidSwitchExternalPower = "ignore";
+   services.logind.settings.Login = {
+   HandleLidSwitchExternalPower = "ignore";
+   HandleLidSwitch = "ignore";
+   KillUserProcesses = false;
+   };
+
   zramSwap.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.void = {
     isNormalUser = true;
     description = "void";
-    extraGroups = [ "networkmanager" "wheel" "video" "audio" "storage" "optical" "input"];
+    extraGroups = [ "wheel" "video" "audio" "storage" "optical" "input" "seat" ];
     packages = with pkgs; [
-        hyprlandPlugins.hyprbars
-        flat-remix-gtk
-        flat-remix-icon-theme
+     # No user packs for now
     ];
   };
 
- # Allow unfree packages
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # Enable doas instead of sudo
-  # security.doas.enable = true;
-  # security.sudo.enable = false;
+   security.doas.enable = true;
+   security.sudo.enable = false;
+   security.polkit.enable = true;
 
   # Configure doas
-  # security.doas.extraRules = [{
-  #	users = [ "void" ];
-  #	keepEnv = true;
-  #     persist = true;
-  #}];
+   security.doas.extraRules = [{
+  	users = [ "void" ];
+  	keepEnv = true;
+        persist = true;
+  }];
 
   # Enable packages
-  programs.firefox.enable = true;
   programs.udevil.enable = true;
   programs.waybar.enable = true;
-  programs.regreet.enable = true;
-
+  programs.hyprlock.enable = true;  
   programs.hyprland = {
-  enable = true;
+    enable = true;
   }; 
 
   # Default shell
-  # users.defaultUserShell = pkgs.mksh;
-
+  users.defaultUserShell = pkgs.mksh;
+  
+  # Dash as sh
+  environment.binsh = "${pkgs.dash}/bin/dash";
+ 
   # Intel configuration
   hardware.graphics = {
     enable = true;
@@ -121,19 +148,31 @@
     ];
   };
 
+  # Remove defalt packages
+  programs.nano.enable = false;
+  programs.xwayland.enable = false;
+
   # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  audacity
+  nvi
+  dash
+  iwgtk
   cage
-  dunst
-  deadbeef
-  swww
+  pragha
+  brave 
+  ffmpeg-full
+  mako
   fastfetch
+  gimp3
+  transmission_4
+  hyprlandPlugins.hyprbars
+  adwaita-icon-theme
+  flat-remix-icon-theme
+  nwg-look
+  swww
   gsimplecal
-  greetd.gtkgreet
+  gtkgreet
+  glib
   wofi
   foot
   wf-recorder
@@ -147,21 +186,14 @@
   grim
   slurp
   gucharmap
-  ffmpeg-full
-  gimp
-  transmission_3
-  telegram-desktop
   unzip
   p7zip
-  dash
   pavucontrol
   pcmanfm
+  ffmpegthumbnailer
   python3
-  plymouth
-  swaylock
-  waypaper
-  xfce.mousepad
-  shotcut
+  waytrogen
+  kotatogram-desktop
   ];
 
   # Set session environment
@@ -173,45 +205,21 @@
     LIBVA_DRIVER_NAME = "iHD";
     # Not officially in the specification
     XDG_BIN_HOME    = "$HOME/.local/bin";
-    PATH = [ 
-      "${XDG_BIN_HOME}"
-    ];
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
+    PATH = "$PATH:${XDG_BIN_HOME}";
+  };
+ 
+  # Unbloat system
+  nix.optimise.automatic = true;
+
+  nix.settings = {
+    auto-optimise-store = true;
+    warn-dirty = false;
+    experimental-features = [ "nix-command" "flakes" ];
   };
 
-  programs.bash.shellAliases = {
-  ls = "ls --color=auto";
-  myip = "curl ident.me;echo";
-  weather = "curl -4 http://wttr.in/Sao-Paulo";
-  mynix = "clear; fastfetch; lsblk -f;";
-  nixed = "sudo nano /etc/nixos/configuration.nix";
-  nixup = "sudo nixos-rebuild switch --upgrade";
-  nixrm = "sudo nix-collect-garbage --delete-old;sudo /run/current-system/bin/switch-to-configuration boot";
-  nixs = "nix-env -qaP";
-  nixsh = "nix-shell -p";
-  nixr = "nix-collect-garbage";
-  nixlsg = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
-  hyprc = "nano /home/void/.config/hypr/hyprland.conf";
-  Nix = "clear ; fastfetch";
-  };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  services.avahi.enable = false;
+  services.printing.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -219,5 +227,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+
+  system.stateVersion = "25.11"; # Did you read the comment?
 }
